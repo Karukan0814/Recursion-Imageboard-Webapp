73ff53953458ff9f2ab0e22c2e6c51d120261eb0
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-03-25 14:54:45 -0700

    出力バッファリング追加

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+
+ob_start(); // 出力バッファリングを開始
+header("Access-Control-Allow-Origin: *");
+
 spl_autoload_extensions(".php");
 spl_autoload_register();
 
@@ -53,7 +58,7 @@ if (isset($routes[$path])) {
 
             if ($sanitized_value && $sanitized_value === $value) {
                 header("{$name}: {$sanitized_value}");
-                // header("Access-Control-Allow-Origin: *");
+                header("Access-Control-Allow-Origin: *");
             } else {
                 // ヘッダー設定に失敗した場合、ログに記録するか処理します。
                 // エラー処理によっては、例外をスローするか、デフォルトのまま続行することもできます。
